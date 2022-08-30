38e5cff2d0e51528f6f8703ae5c5a353feee4b3c
gibewevi <gibewevi@skiff.com>
2022-08-30 14:29:12 +0200

    front update

---
+++ pages/components/Header.js
@@ -0,0 +1,22 @@
+export default function Header(){
+    return(
+            <header className="w-full bg-zinc-900">
+                <div className="h-[50px] max-w-7xl mx-auto flex flex-row justify-center justify-between items-center">
+                    <div className="flex flex-row justify-center items-center">
+                        <div className="animate-spin mx-3 bg-white w-6 h-6 rounded-full border-[5px] border-zinc-900 border-r-cyan-400"></div>
+                        <span className="text-white font-bold">Status : RegisteredOpen</span>
+                    </div>
+                    <button className="bg-white p-1 px-2 rounded-lg font-bold text-orange-800">Metamask</button>
+                </div>
+                <div className="h-[300px] bg-header-style w-full">
+                    <div className="max-w-7xl mx-auto h-full flex flex-row justify-center">
+                        <h1 className="flex flex-col text-center justify-center items-center">
+                            <span className="font-black text-5xl text-white">BLOCKCHAIN VOTING SYSTEM</span>
+                            <span className="font-lighter text-xl tracking-widest ">WEB3 DECENTRALIZED</span>
+                        </h1>
+                       <img src="./images/decentralized.png" className="w-[400px] h-[400px]"></img>
+                    </div>
+                </div>
+            </header>
+        )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        spin: "spin 3s linear infinite",
+      },
+      keyframes: {
+      }
+    },
+  },
+  plugins: [],
+}
