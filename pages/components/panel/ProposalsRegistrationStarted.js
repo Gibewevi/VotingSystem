fc0832c8ce6df2c329db275ac23c3240c1cb48cb
gibewevi <gibewevi@skiff.com>
2022-09-01 23:24:58 +0200

    update proposal + correction bugs event contract

--- pages/components/panel/ProposalsRegistrationStarted.js
+++ pages/components/panel/ProposalsRegistrationStarted.js
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 
 export default function ProposalsRegistrationStarted(props){
 
+    // [isRegister,setIsRegister] = useState(null);
+
+    // const getIsRegister = async() => {
+    //     const signer = provider.getSigner();
+    //     const contract = new ethers.Contract(props.contractAddress, Contract.abi, signer);
+    // }
     // Recuperer getregistered pour afficher le message :
     // - Vous n'etes pas enregistrer/ vous etes enregistrer
     // - patientez a la prochaine session.
@@ -9,10 +15,28 @@ export default function ProposalsRegistrationStarted(props){
 
     return(
             <section className="max-w-7xl mx-auto mt-[125px] flex flex-row justify-center items-center justify-between p-5">
+
+{/* {(() => {
+            switch(isRegister) {
+                case false:
+                    return (
+                     
+                        )
+                case true:
+                    return (
+                     
+                        )
+                case null:
+                    return <span>NULL</span>
+                default:
+                return <span>DEFAULT</span>
+            }
+            })()} */}
+
                     <div className="w-full border border-slate-250 rounded-lg shadow-lg h-full p-5 flex flex-col">
                         <h1 className="flex flex-row">
-                            <span className="font-bold text-5xl text-slate-800 ">Congratulations!</span>
-                            <span className=" mx-5 font-semibold text-5xl text-teal-600 tracking-wide">You are registered.</span>
+                            <span className="font-bold text-4xl text-slate-800 ">Congratulations!</span>
+                            <span className=" mx-5 font-semibold text-4xl text-teal-600 tracking-wide">You are registered.</span>
                         </h1>
                         <span className="font-semibold text-2xl text-slate-800 tracking-widest mt-8">Quick and easy.</span>
                         <div className="flex flex-col">
--- pages/components/panel/RegisteringVoters.js
+++ pages/components/panel/RegisteringVoters.js
@@ -9,23 +9,26 @@ export default function RegisteringVoters(props){
     const contractAddress = props.contractAddress;
     const [buttonAccount, setButtonAccount] = useState(false);
     const [buttonStatusRegistered, setButtonStatusRegistered] = useState(false);
+    const [isRegister, setIsRegister] = useState(false);
     const toast = useToast();
 
     useEffect(()=>{
         if(account){
+            getIsRegister();
             eventRegistered();
             setButtonAccount(true);
         } else (setButtonAccount(false))
     })
 
-    const GetButtonStatusRegister = async() => {
-    const contract = new ethers.Contract(contractAddress, Contract.abi, provider);
-    console.log(account);
-    let transaction = await contract.getRegisteringVoters(account);
-    console.log(transaction);
+    const getIsRegister = async() => {
+        const contract = new ethers.Contract(contractAddress, Contract.abi, provider);
+        let transaction = await contract.getRegisteringVoters(account);
+        setIsRegister(transaction);
+        console.log("Voter register is "+transaction);
     }
 
     const eventRegistered = async() =>{
+        // Retrieve registration events
         const contract = new ethers.Contract(contractAddress, Contract.abi, provider);
             contract.on("isRegistering",(address,isRegistering, hasVoted, votedProposalID)=>{
             console.log('------ Event isRegistering ------');
@@ -38,6 +41,7 @@ export default function RegisteringVoters(props){
     }
 
     const RegisteringVoters = async() =>{
+        // Register the voter
         const signer = provider.getSigner();
         const contract = new ethers.Contract(props.contractAddress, Contract.abi, signer);
 
@@ -65,24 +69,48 @@ export default function RegisteringVoters(props){
     return(
             <section className="max-w-7xl mx-auto mt-[125px] flex flex-row justify-center items-center justify-between p-5">
 
-                <div className="border border-slate-250 rounded-lg shadow-lg h-full w-2/3 p-5 flex flex-col">
-                    <h1 className="font-bold text-5xl text-slate-800 tracking-wider">Getting Started !</h1>
-                    <span className="font-semibold text-2xl text-slate-800 tracking-wider mt-8">Quick and easy.</span>
-                    <div className="flex flex-col">
-                      <span className="mb-2 font-lighter text-xl text-slate-800 tracking-wider mt-5">Participate in your first decentralized vote. It's quick and easy! Connect your</span>
-                      <button className="bg-teal-500 p-1 px-2 rounded-lg font-bold text-white text-xl max-w-[120px]">Metamask</button>
-                      <span className="font-lighter text-xl text-slate-800 tracking-wider mt-5">Sign up for the whitelist to participate in the next vote!</span>
-                    </div>
-
-                    <div className="mt-5 border border-orange-">
-                        <button onClick={GetButtonStatusRegister} className="float-left bg-slate-400 max-w-[120px] p-2 rounded-lg font-black text-lg text-white">Waiting...</button>
-                        {buttonAccount ? 
-                        <button onClick={RegisteringVoters} className="float-right mr-3 bg-sky-500 max-w-[120px] p-2 rounded-lg font-black text-lg text-white">Registering</button>
-                        :
-                        <span></span>}
-                    </div>
-
-                </div>
+            {(() => {
+            switch(isRegister) {
+                case false:
+                    return (
+                        <div className="border border-slate-250 rounded-lg shadow-lg h-full w-2/3 p-5 flex flex-col">
+                        <h1 className="font-bold text-4xl text-slate-800 tracking-wider">Getting Started !</h1>
+                        <span className="font-semibold text-2xl text-slate-800 tracking-wider mt-8">Quick and easy.</span>
+                        <div className="flex flex-col">
+                          <span className="mb-2 font-lighter text-xl text-slate-800 tracking-wider mt-5">Participate in your first decentralized vote. It's quick and easy! Connect your</span>
+                          <button className="bg-teal-500 p-1 px-2 rounded-lg font-bold text-white text-xl max-w-[120px]">Metamask</button>
+                          <span className="font-lighter text-xl text-slate-800 tracking-wider mt-5">Sign up for the whitelist to participate in the next vote!</span>
+                        </div>
+    
+                        <div className="mt-5">
+                            {buttonAccount ? 
+                            <button onClick={RegisteringVoters} className="float-right mr-3 bg-sky-500 max-w-[120px] p-2 rounded-lg font-black text-lg text-white">Registering</button>
+                            :
+                            <span></span>}
+                        </div>
+    
+                    </div>                         
+                        )
+                case true:
+                    return (
+                        <div className="border border-slate-250 rounded-lg shadow-lg h-full w-2/3 p-5 flex flex-col">
+                            <div className="flex flex-row">
+                                 <h1 className="font-bold text-4xl text-slate-800 tracking-wider">Getting Started !</h1>
+                                 <img src="./images/send.svg" className="w-8 h-8 mt-1 mx-2"></img>
+                            </div>
+                            <span className="font-semibold text-2xl text-slate-800 tracking-wider mt-8">Quick and easy.</span>
+                        <div className="flex flex-col">
+                          <span className="mb-2 font-lighter text-xl text-slate-800 tracking-wider mt-5">You are already registered for the next decentralized voting session.</span>
+                          <span className="font-lighter text-xl text-slate-800 tracking-wider mb-5">Please wait, and you can make proposals!</span>
+                        </div>
+                    </div>                       
+                        )
+                case null:
+                    return <span>NULL</span>
+                default:
+                return <span>DEFAULT</span>
+            }
+            })()}
                 <div className="h-[150px] w-1/3"></div>
             </section>
         )
